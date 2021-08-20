34f243e77de59c43d52d934c56d2cd9fdf10b3cf
Michael Winser <mike.winser@gmail.com>
2021-08-19 21:51:37 -0700

    added category buttons which filter list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,64 @@
 import * as React from "react"
-import { Link } from "gatsby"
 import { graphql } from "gatsby"
+import { useState } from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = ({data}) => (
+function IndexPage({data}){ 
+  const [filter, setFilter] = useState()
+  return(
   <Layout>
     <Seo title="Home" />
     <h1>Improv Games List</h1>
-    {data.allGoogleSheet.nodes[0].Main.map(node=>
+    {filter
+      ? <>
+          <p>Filtered by: {filter}</p>
+          <button onClick={()=>setFilter()}>Clear Filter</button>
+        </>
+      : null
+    }
+    {data
+      .allGoogleSheet
+      .nodes[0]
+      .Main
+      .filter(node=>filter ? node.category.includes(filter) : true)
+      .map(node=>
       (<div key={node.id} className="card">
         <h2>{node.name}</h2>
-        <p><strong>Categories:</strong> {node.category}</p>
+        
+        <p>
+          <strong>Categories: </strong> 
+          {node.category.split(",").map(item=>
+              <button onClick={()=>setFilter(item.trim())}>
+                {item.trim()}
+              </button>
+
+            )
+          }
+        </p>
         <p><strong>Players:</strong> {node.players}</p>
         <p><strong>Description:</strong> {node.description}</p>
         <p><strong>How to play:</strong> {node.howToPlay}</p>
-        <p><strong>Notes:</strong> {node.notes}</p>
+        {node.example
+        ? <blockquote>{node.example}</blockquote>
+        : null
+        }
+        
+        {node.notes 
+        ? <p><strong>Notes:</strong> {node.notes}</p>
+        : null
+        }
+        {node.seeAlso 
+        ? <p><strong>See also:</strong> {node.seeAlso}</p>
+        : null
+        }
       </div>)
     ) 
     }
-    
-    
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
   </Layout>
 )
+}
 
 export default IndexPage
 
@@ -37,13 +68,14 @@ query AllGames{
   allGoogleSheet {
     nodes {
       Main {
-        id
         name
         howToPlay
         description
         category
         notes
         players
+        example
+        seeAlso
       }
     }
   }
