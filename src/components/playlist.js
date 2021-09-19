3686db6e472f932b7b556a6509aafa98f2ef4369
Michael Winser <mike.winser@gmail.com>
2021-09-19 12:03:53 -0700

    remove unused Link

--- src/components/playlist.js
+++ src/components/playlist.js
@@ -4,7 +4,6 @@ import { useContext } from "react"
 import { Context } from "../context"
 import { useState } from "react"
 import { useReactToPrint } from "react-to-print"
-import { Link } from "gatsby"
 
 
 
